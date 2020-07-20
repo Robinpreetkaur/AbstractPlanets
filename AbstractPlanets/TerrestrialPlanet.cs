755f9d738f2cdf7228af30e9f25113fa7181337a
Robinpreet <rkaur750@my.centennialcollege.ca>
2020-07-20 16:59:44 -0400

    TerrestrialPlanet.cs is created(point 3)

--- AbstractPlanets/TerrestrialPlanet.cs
+++ AbstractPlanets/TerrestrialPlanet.cs
@@ -4,8 +4,38 @@ using System.Text;
 
 namespace AbstractPlanets
 {
-    class TerrestrialPlanet
+    class TerrestrialPlanet:Planet,IHasMoons,IHabitable
     {
+
+        //Private instance variable
+        private bool m_oxygen;
+
+
+        //Constructor 
+
+        public TerrestrialPlanet(string name, double diameter, double mass, bool oxygen): 
+            base(name, diameter, mass)
+        {
+            m_oxygen = oxygen;
+
+        }
+
+
+        //Public Methods 
+
+        public bool HasMoons()
+        {
+            bool hasMoons = MoonCount > 0 ? true : false;
+            return hasMoons;
+        }
+
+        public bool Habitable()
+        {
+            bool isHabitable = m_oxygen == true ? true : false;
+            return isHabitable;
+        }
+
+
 
 
 
